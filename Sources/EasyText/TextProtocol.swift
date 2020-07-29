db53ac6fb9bc0b875c1ea121967de84beed1c68e
Yuta Koshizawa <koher@koherent.org>
2020-07-30 01:04:06 +0900

    Make `TextProtocol` inherit from `Collection`

--- Sources/EasyText/TextProtocol.swift
+++ Sources/EasyText/TextProtocol.swift
@@ -1,12 +1,2 @@
-public protocol TextProtocol: Sequence, CustomStringConvertible, CustomDebugStringConvertible, ExpressibleByStringLiteral {
-    subscript(index: Int) -> Character { get set }
-    
-    var count: Int { get }
-    var startIndex: Int { get }
-    var endIndex: Int { get }
-    var first: Character? { get }
-    var last: Character? { get }
-
-    subscript(bounds: Range<Int>) -> TextSlice { get set }
-    subscript(bounds: ClosedRange<Int>) -> TextSlice { get set }
+public protocol TextProtocol: RandomAccessCollection, RangeReplaceableCollection, MutableCollection, CustomStringConvertible, CustomDebugStringConvertible, ExpressibleByStringLiteral where Element == Character, Index == Int {
 }
